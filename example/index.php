e108fc91a41b6a1784fc8558297a9de6d6f0c589
Aaron McConnell <amcconnell@duosecurity.com>
2022-02-04 10:35:47 -0500

    Add clarifying remarks about session storage to example

--- example/index.php
+++ example/index.php
@@ -68,6 +68,10 @@ $app->post('/', function (Request $request, Response $response, $args) use ($app
 
     # Generate random string to act as a state for the exchange.
     # Store it in the session to be later used by the callback.
+    # This example demonstrates use of the http session (cookie-based) 
+    # for storing the state. In some applications, strict cookie 
+    # controls or other session security measures will mean a different
+    # mechanism to persist the state and username will be necessary.
     $state = $duo_client->generateState();
     $session = new \SlimSession\Helper();
     $session->set("state", $state);
